440ec5e529416e5b3d6f75ddac1104eb7d65985c
marcolax22 <86615981+marcolax22@users.noreply.github.com>
2021-08-15 11:38:19 +0200

    Changes code

--- datamanagement_PostgreSQL.R
+++ datamanagement_PostgreSQL.R
@@ -1,1 +1,14 @@
 #This is the PostgreSQL Code
+
+#loading packages---------------------------------------------------------------
+library(DBI)
+library(RPostgres)
+
+#connect to the database--------------------------------------------------------
+db <- dbConnect(RPostgres::Postgres(),
+                dbname = "dataproject",
+                user="dpuser",
+                password="dppassword")
+
+#loading the dataset------------------------------------------------------------
+
