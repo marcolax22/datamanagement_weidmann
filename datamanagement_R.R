56039b468a6b511c3bfaaf89544e3df281493c60
marcolax22 <86615981+marcolax22@users.noreply.github.com>
2021-08-30 16:34:41 +0200

    New data

--- datamanagement_R.R
+++ datamanagement_R.R
@@ -3,8 +3,19 @@
 #loading packages---------------------------------------------------------------
 library(tidyverse)
 library(naniar)
+library(readr)
+library(countrycode)
 
-#loading the dataset------------------------------------------------------------
+#loading the dataset from ParlGov-----------------------------------------------
+party <- read.csv(file.path("data","view_party.csv"))
+election <- read.csv(file.path("data","view_election.csv"))
+cabinet <- read.csv(file.path("data","view_cabinet.csv"), encoding = "utf-8")
 
+#loading the dataset from EuroStat----------------------------------------------
+revenue <- read_tsv(file.path("data","tec00021.tsv"))
+expenditure <- read_tsv(file.path("data","tec00023.tsv"))
+social_benefits <- read_tsv(file.path("data","tec00026.tsv"))
+taxes <- read_tsv(file.path("data","tec00018.tsv"))
+social_contribution <- read_tsv(file.path("data","tec00019.tsv"))
 
-#select variables---------------------------------------------------------------
+#structure of the data----------------------------------------------------------
